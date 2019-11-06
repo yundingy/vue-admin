a2cbd8020d07c0ccc0af1b3d50ad4f4192116f94
yundingy <yunding@vip.qq.com>
2019-11-06 10:42:49 +0800

    table测试

--- src/views/table/basic.vue
+++ src/views/table/basic.vue
@@ -1,122 +1,99 @@
-<style lang='less'>
-  .table-basic-vue {
-  }
-</style>
 <template>
-  <div class="table-basic-vue frame-page h-panel">
-    <div class="h-panel-bar"><span class="h-panel-title">基础表格</span></div>
-    <div class="h-panel-body">
-      <div>
-        <Tabs :datas="tabs" className="common-status-filter-tabs" v-model="type" @change="getData(true)">
-          <template slot-scope="{tab}" slot="item">
-            <p class="code">{{counts[`${tab.key}`] || 0}}</p>
-            <p class="name">{{tab.title}}</p>
-          </template>
-        </Tabs>
-      </div>
-      <div class="common-filter-bar">
-        <div class="float-right">
-          <Select class="common-filter-select" autosize v-width="160" v-model="sort" :datas="sortList" :no-border="true"
-                  @input="getData(true)" :null-option="false" placeholder="请选择您的内容"></Select>
-        </div>
-      </div>
-      <Table :loading="loading" :datas="datas">
-        <TableItem title="序号">
-          <template slot-scope="{index}">{{index+1}}</template>
-        </TableItem>
-        <TableItem :width="200" prop="name" title="姓名"></TableItem>
-        <TableItem :width="200" prop="salary" title="薪资"></TableItem>
-        <TableItem :width="200" prop="country" title="国家"></TableItem>
-        <TableItem :width="200" prop="city" title="城市"></TableItem>
-        <TableItem :width="200" prop="name" title="姓名"></TableItem>
-        <TableItem :width="200" prop="salary" title="薪资"></TableItem>
-        <TableItem :width="200" prop="country" title="国家"></TableItem>
-        <TableItem :width="200" prop="city" title="城市"></TableItem>
-        <TableItem :width="200" prop="name" title="姓名"></TableItem>
-        <TableItem :width="200" prop="salary" title="薪资"></TableItem>
-        <TableItem :width="200" prop="country" title="国家"></TableItem>
-        <TableItem :width="200" prop="city" title="城市"></TableItem>
-      </Table>
-      <p></p>
-      <Pagination v-if="pagination.total>0" align="right" v-model="pagination" @change="changePage"/>
-    </div>
+  <div>
+    <p><SwitchList v-model="mode" small :datas="{'checkbox': '多选', 'rowSelect': '单选'}"></SwitchList></p>
+    <p v-if="mode=='rowSelect'">
+      <h-switch v-model="useRadio" small>使用Radio / Use radio</h-switch>
+      <Button size="s" @click="setRowSelect">选中第一行 / Select the first line</Button>
+      <Button size="s" @click="reset">清空选择 / Clear</Button>
+    </p>
+    <p v-else>
+      <Button size="s" @click="invereSelection">反向选择 / Inverse selection</Button>
+      <Button size="s" @click="setOddSelection">选择奇数列 / Select odd columns</Button>
+      <Button size="s" @click="reset">清空选择 / Clear</Button>
+    </p>
+    <Table :datas="datas" ref="table" :height="400" @select="onselect" :checkbox="mode=='checkbox'" :selectRow="mode=='rowSelect'" :radio="mode=='rowSelect' && useRadio" selectWhenClickTr>
+      <TableItem title="ID" prop="id" align="center" :width="80" fixed="left"></TableItem>
+      <TableItem title="age" prop="age" :width="150"></TableItem>
+      <TableItem title="address" prop="address" align="center" :width="150"></TableItem>
+      <TableItem title="name" prop="name" :width="150"></TableItem>
+      <TableItem title="age" prop="age" :width="150"></TableItem>
+      <TableItem title="address" prop="address" align="center" :width="150"></TableItem>
+      <TableItem title="name" prop="name" :width="150"></TableItem>
+      <TableItem title="age" prop="age" :width="150"></TableItem>
+      <TableItem title="address" prop="address" align="center" :width="150"></TableItem>
+      <TableItem title="operating" align="center" :width="80" fixed="right"><template slot-scope="{data}"><button class="h-btn h-btn-s h-btn-red" @click="remove(datas, data)"><i class="h-icon-trash"></i></button></template></TableItem>
+      <div slot="empty">Custom reminder: no data at this time</div>
+    </Table>
   </div>
 </template>
+
 <script>
   export default {
     data() {
       return {
-        keyword: '',
-        sort: 'updatedAt:desc',
-        sortList: {
-          'updatedAt:desc': '更新时间倒序',
-          'updatedAt:asc': '更新时间正序',
-          'createdAt:desc': '创建时间倒序',
-          'createdAt:asc': '创建时间正序'
-        },
-        pagination: {
-          page: 1,
-          size: 20,
-          total: 0
-        },
-        tabs: [
-          { key: 'China', title: 'Malawi' },
-          { key: 'Niger', title: 'Niger' },
-          { key: 'Curaçao', title: 'Curaçao' },
-          { key: 'Korea', title: 'Korea' },
-          { key: 'Malawi', title: 'Malawi' }
-        ],
-        type: 'China',
         datas: [],
-        counts: {},
-        loading: false
+        useRadio: false,
+        mode: 'checkbox'
       }
     },
     mounted() {
-      this.init()
+      setTimeout(() => {
+        this.datas = [
+          { id: 5, name: 'Test 5', age: 12, address: 'Shanghai' },
+          { id: 6, name: 'Test 6', age: 12, address: 'Shanghai', _disabledSelect: true },
+          { id: 7, name: 'Test 7', age: 12, address: 'Shanghai' },
+          { id: 5, name: 'Test 5', age: 12, address: 'Shanghai' },
+          { id: 6, name: 'Test 6', age: 12, address: 'Shanghai' },
+          { id: 7, name: 'Test 7', age: 12, address: 'Shanghai' },
+          { id: 7, name: 'Test 7', age: 12, address: 'Shanghai' },
+          { id: 5, name: 'Test 5', age: 12, address: 'Shanghai' },
+          { id: 6, name: 'Test 6', age: 12, address: 'Shanghai' },
+          { id: 7, name: 'Test 7', age: 12, address: 'Shanghai' },
+          { id: 7, name: 'Test 7', age: 12, address: 'Shanghai' },
+          { id: 7, name: 'Test 7', age: 12, address: 'Shanghai' },
+          { id: 5, name: 'Test 5', age: 12, address: 'Shanghai' },
+          { id: 6, name: 'Test 6', age: 12, address: 'Shanghai' },
+          { id: 7, name: 'Test 7', age: 12, address: 'Shanghai' }
+        ]
+      }, 100)
+
+      setTimeout(() => {
+        this.datas = [
+          { id: 5, name: 'Test 5', age: 12, address: 'Shanghai' },
+          { id: 6, name: 'Test 6', age: 12, address: 'Shanghai', _disabledSelect: true },
+          { id: 7, name: 'Test 7', age: 12, address: 'Shanghai' },
+          { id: 5, name: 'Test 5', age: 12, address: 'Shanghai' },
+          { id: 6, name: 'Test 6', age: 12, address: 'Shanghai' },
+          { id: 7, name: 'Test 7', age: 12, address: 'Shanghai' },
+          { id: 7, name: 'Test 7', age: 12, address: 'Shanghai' }
+        ]
+      }, 1000)
     },
     methods: {
-      init() {
-        this.getData()
-        this.getCounts()
+      setRowSelect() {
+        this.$refs.table.setRowSelect(this.datas[0])
       },
-      changePage() {
-        this.getData(true)
+      reset() {
+        this.$refs.table.clearRowSelect()
+        this.$refs.table.setSelection([])
       },
-      getCounts() {
-        setTimeout(() => {
-          this.counts = {
-            China: 900,
-            Niger: 90,
-            Curaçao: 20,
-            Korea: 30,
-            Malawi: 45
-          }
-        }, 1000)
+      setOddSelection() {
+        this.$refs.table.setSelection(
+          this.datas.filter((item, index) => (index + 1) % 2 == 1)
+        )
       },
-      getData(reload = false) {
-        if (reload) {
-          this.pagination.page = 1
-        }
-        this.loading = true
-        setTimeout(() => {
-          this.datas = [{ name: 'Dakota Rice', salary: '$36,738', country: 'Niger', city: 'Oud-Turnhout' },
-            { name: 'Minerva Hooper', salary: '$23,789', country: 'Curaçao', city: 'Sinaai-Waas' },
-            { name: 'Sage Rodriguez', salary: '$56,142', country: 'Netherlands', city: 'Baileux' },
-            { name: 'Philip Chaney', salary: '$38,735', country: 'Korea, South', city: 'Overland Park' },
-            { name: 'Doris Greene', salary: '$63,542', country: 'Malawi', city: 'Feldkirchen in Kärnten' },
-            { name: 'Mason Porter', salary: '$78,615', country: 'Chile', city: 'Gloucester' },
-            { name: 'Dakota Rice', salary: '$36,738', country: 'Niger', city: 'Oud-Turnhout' },
-            { name: 'Minerva Hooper', salary: '$23,789', country: 'Curaçao', city: 'Sinaai-Waas' },
-            { name: 'Sage Rodriguez', salary: '$56,142', country: 'Netherlands', city: 'Baileux' },
-            { name: 'Philip Chaney', salary: '$38,735', country: 'Korea, South', city: 'Overland Park' },
-            { name: 'Doris Greene', salary: '$63,542', country: 'Malawi', city: 'Feldkirchen in Kärnten' },
-            { name: 'Mason Porter', salary: '$78,615', country: 'Chile', city: 'Gloucester' }]
-          this.pagination.total = 100
-          this.loading = false
-        }, 1000)
+      remove(datas, data) {
+        datas.splice(datas.indexOf(data), 1)
+      },
+      add(datas) {
+        datas.push({ id: 7, name: 'Add to', age: 12, address: 'Then added' })
+      },
+      invereSelection() {
+        this.$refs.table.invereSelection()
+      },
+      onselect(data, event) {
+        log('onselect', data, event)
       }
-    },
-    computed: {}
+    }
   }
 </script>
